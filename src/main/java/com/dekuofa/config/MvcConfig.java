45dc5ff2743436a9126c25c8b32b035e53affa9a
xiwenAndlejian <94333236@qq.com>
2018-08-23 16:51:32 +0800

    :heavy_plus_sign: 添加swagger & swagger-ui 接口 访问路径 /swagger-ui.html

---
+++ src/main/java/com/dekuofa/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.dekuofa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author gx <br>
+ * @date 2018-08-23 <br>
+ */
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//    }
+//}
