9845e4b6643a194dc8283754d8818ef98e665cdc
xiwenAndlejian <94333236@qq.com>
2018-08-24 16:14:07 +0800

    全局参数异常处理

--- src/main/java/com/dekuofa/config/GlobalExceptionHandler.java
+++ src/main/java/com/dekuofa/config/GlobalExceptionHandler.java
@@ -2,10 +2,15 @@ package com.dekuofa.config;
 
 import com.dekuofa.model.response.RestResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 全局异常处理
@@ -17,8 +22,19 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class GlobalExceptionHandler {
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public RestResponse<?> validatorHandler(MethodArgumentNotValidException e) {
+        Map<String, String> errorMsg = new HashMap<>();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errorMsg.put(error.getField(), error.getDefaultMessage());
+        }
+        return RestResponse.fail("参数异常").payload(errorMsg);
+    }
+
     @ExceptionHandler(Exception.class)
     public RestResponse<?> exceptionHandler(HttpServletRequest request, Exception e) {
+        e.printStackTrace();
         return RestResponse.fail(e.getMessage());
     }
 
