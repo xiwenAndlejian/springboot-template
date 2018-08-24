d4643e603fb04431c1c6d8e3eb1a55bc6c820f70
xiwenAndlejian <94333236@qq.com>
2018-08-24 16:14:34 +0800

    备注

--- src/main/java/com/dekuofa/model/BaseUserInfo.java
+++ src/main/java/com/dekuofa/model/BaseUserInfo.java
@@ -3,6 +3,8 @@ package com.dekuofa.model;
 import com.dekuofa.model.enums.UserType;
 
 /**
+ * fixme 自定义handler处理接口，
+ *
  * @author gx <br>
  * @date 2018-08-21 <br>
  */
@@ -16,4 +18,6 @@ public interface BaseUserInfo {
     int getUserId();
 
     boolean isEmpty();
+
+    boolean isCurrentUser(int userId);
 }
