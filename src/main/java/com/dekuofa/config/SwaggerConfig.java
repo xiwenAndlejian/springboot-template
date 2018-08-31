1410327fb1d748ab6a944bd9e6d4d80d47add94d
xiwenAndlejian <94333236@qq.com>
2018-08-31 08:38:19 +0800

    :wrench: 修改swagger2 配置，增加全局 token head

--- src/main/java/com/dekuofa/config/SwaggerConfig.java
+++ src/main/java/com/dekuofa/config/SwaggerConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author dekuofa <br>
@@ -21,12 +25,37 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api(ApiInfo apiInfo) {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(securityContexts())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    // swagger2 全局使用 jwt 配置 参照 http://fish119.site/2018/01/04/Swagger2-%E9%9D%9E%E5%85%A8%E5%B1%80Header%E5%8F%82%E6%95%B0%EF%BC%88Token%EF%BC%89%E9%85%8D%E7%BD%AE/
+    private List<SecurityContext> securityContexts() {
+        return Collections.singletonList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!login).*$"))
+                        .build()
+        );
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<AuthorizationScope> authorizationScopes = new ArrayList<>();
+        authorizationScopes.add(new AuthorizationScope("global", "accessEverything"));
+        return Collections.singletonList(
+                new SecurityReference("Authorization",
+                        (AuthorizationScope[]) authorizationScopes.toArray()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 
     @Bean
