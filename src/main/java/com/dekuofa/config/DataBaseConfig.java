660a8778ef5c4b3632dbd33e31da14bc97c419ef
xiwenAndlejian <94333236@qq.com>
2018-11-12 15:48:42 +0800

    :bug: 枚举 converter 增加

--- src/main/java/com/dekuofa/config/DataBaseConfig.java
+++ src/main/java/com/dekuofa/config/DataBaseConfig.java
@@ -1,26 +1,16 @@
 package com.dekuofa.config;
 
-import com.dekuofa.model.enums.BaseStatus;
 import com.dekuofa.utils.BaseCodeEnumConverterFactory;
 import io.github.biezhi.anima.Anima;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
 import org.sql2o.converters.Converter;
-import org.sql2o.converters.java8.LocalDateConverter;
-import org.sql2o.converters.java8.LocalDateTimeConverter;
-import org.sql2o.quirks.NoQuirks;
-import org.sql2o.quirks.Quirks;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author dekuofa <br>
@@ -36,10 +26,13 @@ public class DataBaseConfig {
         env = environment;
     }
 
+    // todo convert 泛型增加优化
     @Bean
     public Converter[] converters() {
         List<Converter<?>> converters = new ArrayList<>();
         converters.add(BaseCodeEnumConverterFactory.newBaseCodeConverter());
+        converters.add(BaseCodeEnumConverterFactory.newGenderConvert());
+        converters.add(BaseCodeEnumConverterFactory.newUserTypeConvert());
         return converters.toArray(new Converter[]{});
     }
 
--- src/main/java/com/dekuofa/utils/BaseCodeEnumConverterFactory.java
+++ src/main/java/com/dekuofa/utils/BaseCodeEnumConverterFactory.java
@@ -1,6 +1,8 @@
 package com.dekuofa.utils;
 
 import com.dekuofa.model.enums.BaseCodeEnum;
+import com.dekuofa.model.enums.Gender;
+import com.dekuofa.model.enums.UserType;
 import org.sql2o.converters.Converter;
 import org.sql2o.converters.ConverterException;
 
@@ -13,6 +15,8 @@ public class BaseCodeEnumConverterFactory {
     public static <E extends Enum & BaseCodeEnum> Converter<E> newConverter(final Class<E> enumType) {
 
         return new Converter<E>() {
+
+
             @SuppressWarnings("unchecked")
             @Override
             public E convert(Object val) throws ConverterException {
@@ -61,6 +65,87 @@ public class BaseCodeEnumConverterFactory {
 
             @Override
             public Object toDatabaseParam(BaseCodeEnum val) {
+                return val.getCode();
+            }
+        };
+    }
+
+    public static <T extends BaseCodeEnum> Converter<T> newCodeConverter(Class<T> clazz) {
+        return new Converter<T>() {
+            @Override
+            public T convert(Object val) throws ConverterException {
+                if (val == null) {
+                    return null;
+                }
+                try {
+                    T[] enums = clazz.getEnumConstants();
+                    for (T e : enums) {
+                        if (val.equals(e.getCode())) {
+                            return e;
+                        }
+                    }
+                } catch (Throwable t) {
+                    throw new ConverterException("Error converting value '" + val.toString() + "' to " + BaseCodeEnum.class.getName(), t);
+                }
+                throw new ConverterException("Cannot convert type '" + val.getClass().getName() + "' to an Enum");
+            }
+
+            @Override
+            public Object toDatabaseParam(T val) {
+                return val.getCode();
+            }
+        };
+    }
+
+    public static Converter<Gender> newGenderConvert() {
+        return new Converter<Gender>() {
+            @Override
+            public Gender convert(Object val) throws ConverterException {
+                if (val == null) {
+                    return null;
+                }
+                try {
+                    Gender[] enums = Gender.class.getEnumConstants();
+                    for (Gender e : enums) {
+                        if (val.equals(e.getCode())) {
+                            return e;
+                        }
+                    }
+                } catch (Throwable t) {
+                    throw new ConverterException("Error converting value '" + val.toString() + "' to " + Gender.class.getName(), t);
+                }
+                throw new ConverterException("Cannot convert type '" + val.getClass().getName() + "' to an Enum");
+            }
+
+            @Override
+            public Object toDatabaseParam(Gender val) {
+                return val.getCode();
+            }
+        };
+    }
+
+    public static Converter<UserType> newUserTypeConvert() {
+        return new Converter<UserType>() {
+            @Override
+            public UserType convert(Object val) throws ConverterException {
+                if (val == null) {
+                    return null;
+                }
+                try {
+                    UserType[] enums = UserType.class.getEnumConstants();
+                    for (UserType e : enums) {
+                        if (val.equals(e.getCode())) {
+                            return e;
+                        }
+                    }
+                } catch (Throwable t) {
+                    throw new ConverterException("Error converting value '" + val.toString() + "' to " + UserType.class.getName(), t);
+                }
+                throw new ConverterException("Cannot convert type '" + val.getClass().getName() + "' to an Enum");
+            }
+
+            @Override
+            public Object toDatabaseParam(UserType val) {
                 return val.getCode();
             }
         };
